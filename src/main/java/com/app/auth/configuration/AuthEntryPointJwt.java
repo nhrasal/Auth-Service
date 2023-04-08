1c0d2d272ebc9078bb128129d630aa07eab53084
NH Rasal <nhrasal.cse@gmail.com>
2023-04-08 11:50:37 +0600

    filterchain add json response

--- src/main/java/com/app/auth/configuration/AuthEntryPointJwt.java
+++ src/main/java/com/app/auth/configuration/AuthEntryPointJwt.java
@@ -1,9 +1,11 @@
 package com.app.auth.configuration;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -12,9 +14,13 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Component
+@Slf4j
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     private static final Logger LOG = LoggerFactory.getLogger(AuthEntryPointJwt.class);
@@ -22,9 +28,23 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+        log.error("Unauthorized error: {}", authException.getMessage());
+
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.getWriter().write("{\"status\": 401, \"body\": null, \"message\": \"Token expired.\"}");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+//        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("body","{}");
+        body.put("meta","{}");
+        body.put("header","{}");
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
     }
 
 }
